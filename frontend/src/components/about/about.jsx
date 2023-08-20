2c75d15086b6fa0f88a77814fb130c7d41988dff
Sarthak Aggarwal <sarthak.agg13@gmail.com>
2023-08-20 10:20:35 -0700

    v1 push

---
+++ frontend/src/components/about/about.jsx
@@ -0,0 +1,17 @@
+import { Container, Typography } from '@mui/material';
+import React from 'react';
+
+const about = () => {
+    return (
+        <Container className='about'>
+            <Typography variant="h3" align="center" style={{ marginBottom: '25px', marginTop: '25px' }}>
+                <b>About Us</b>
+            </Typography>
+            <Typography variant='h5' align='center'>
+                We specializes in training, developing and delivering  software solutions, leveraging cutting-edge technologies to meet the unique needs of our clients in a time bound fashion enabling growth and digital transformation.
+            </Typography>
+        </Container>
+    )
+};
+
+export default about;
